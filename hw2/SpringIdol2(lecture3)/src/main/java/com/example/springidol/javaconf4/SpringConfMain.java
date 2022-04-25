86fe4b5e0fc4b7c0fc956286094ccce649fedebb
HyeJun <solar3070@naver.com>
2022-04-25 20:22:10 +0900

    ssd hw2 1-2

--- hw2/SpringIdol2(lecture3)/src/main/java/com/example/springidol/javaconf4/SpringConfMain.java
+++ hw2/SpringIdol2(lecture3)/src/main/java/com/example/springidol/javaconf4/SpringConfMain.java
@@ -10,6 +10,7 @@ import com.example.springidol.Instrumentalist;
 import com.example.springidol.OneManBand;
 import com.example.springidol.Performer;
 import com.example.springidol.PoeticJuggler;
+import com.example.springidol.Singer;
 import com.example.springidol.SpringIdol;
 
 @Configuration
@@ -21,18 +22,23 @@ public class SpringConfMain {
 	private Instrumentalist instr;		// Instrumentalist 타입 객체가 자동 주입됨
 	@Autowired
 	private OneManBand hank;			// OneManBand 타입 객체가 자동 주입됨
+	@Autowired
+	private Singer chris;			// OneManBand 타입 객체가 자동 주입됨
+	@Autowired
+	private Singer jain;			// OneManBand 타입 객체가 자동 주입됨
 	
 	@Bean
 	public SpringIdol springIdol() {
 		SpringIdol si = new SpringIdol();
-		si.setPerformers(new Performer[]{duke, instr, hank});	// 위 필드를 통해 객체 참조
+		si.setPerformers(new Performer[]{duke, instr, hank, chris, jain});	// 위 필드를 통해 객체 참조
 		return si;		
 	}
 	
 	@Bean
 	public Encore encore() {
 		Encore encore = new Encore();
-		encore.setPerformer(duke);		
+		Performer[] performers = springIdol().getPerformers();
+		encore.setPerformer(performers[(int) (Math.random() * performers.length)]);		// 임의의 performer 선택으로 변경!
 		return encore;		
 	}
 }
--- hw2/SpringIdol2(lecture3)/src/main/java/com/example/springidol/javaconf4/SpringConfSub.java
+++ hw2/SpringIdol2(lecture3)/src/main/java/com/example/springidol/javaconf4/SpringConfSub.java
@@ -5,12 +5,17 @@ import java.util.Properties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.springidol.Cymbal;
 import com.example.springidol.Guitar;
+import com.example.springidol.Harmonica;
 import com.example.springidol.Instrumentalist;
 import com.example.springidol.OneManBand;
+import com.example.springidol.Piano;
 import com.example.springidol.Poem;
 import com.example.springidol.PoeticJuggler;
 import com.example.springidol.Saxophone;
+import com.example.springidol.Singer;
+import com.example.springidol.Song;
 import com.example.springidol.Sonnet29;
 
 @Configuration
@@ -24,6 +29,21 @@ public class SpringConfSub {
 	@Bean
 	public Guitar guitar() {	    
 		return new Guitar();		
+	}
+	
+	@Bean
+	public Harmonica harmonica() {	    
+		return new Harmonica();		
+	}
+	
+	@Bean
+	public Cymbal cymbal() {	    
+		return new Cymbal();		
+	}
+	
+	@Bean
+	public Piano piano() {	    
+		return new Piano();		
 	}
 		
 	@Bean
@@ -40,17 +60,45 @@ public class SpringConfSub {
 	public Instrumentalist instr() {
 		Instrumentalist instr = new Instrumentalist();
 		instr.setSong("Jingle Bells");	
-		instr.setInstrument(saxophone());		// Setter-based DI
+		instr.setInstrument(guitar());		// Setter-based DI
 		return instr;
 	} 
 	
 	@Bean		
 	public OneManBand hank() {
 		Properties props = new Properties();
-		props.setProperty("HARMONICA", "HUM HUM HUM");
+		props.setProperty("GUITAR", "STRUM STRUM STRUM");
 		props.setProperty("CYMBAL", "CRASH CRASH CRASH");
+		props.setProperty("HARMONICA", "HUM HUM HUM");
 		OneManBand omb = new OneManBand();		
 		omb.setInstruments(props);
 		return omb;
-	}		
+	}	
+	
+	@Bean
+	public Singer chris() {
+		return new Singer("Chris Kim", bohemian());
+	}
+	
+	@Bean
+	public Singer jain() {
+		return new Singer("Jain Lee", chris().getSong());
+	}
+	
+	@Bean
+	public Song bohemian() {
+		Song song = new Song();
+		song.setTitle("Bohemian Rhapsody");
+		song.setArtist("Queen");
+		return song;
+	}
+	
+	@Bean
+	public Song someone() {
+		Song song = new Song();
+		song.setTitle("Someone Like You");
+		song.setArtist("Adele");
+		return song;
+	}
+
 }
